e94079e979bd212610fd54bf9cc47d6643e68413
Andres Flores <miguelangelo.fl2011@gmail.com>
2024-04-05 16:41:33 -0400

    agregar apellido

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ app.get('/', function (request, response) {
     response.send(`
     <h1>Hola Mundo!</h1>
     <ul>
-      <li>Nombre: Andres</li>
+      <li>Nombre: Andres Eduardo</li>
+      <li>Apellido: Flores Pedrique</li>
       <li>Cedula: 31.261.566</li>
       <li>Seccion: 2</li>
     </ul>
